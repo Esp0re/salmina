7eead9796cf134c4c265fb5135a45aef026a53a3
Potrac <olivier.grognuz@gmail.com>
2020-06-06 00:49:11 +0200

    css update view scale on mobile login

--- index.php
+++ index.php
@@ -30,6 +30,7 @@
     <head>
         <title>Login</title>
         <link rel="stylesheet" href="styles/clean.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
 
     <body>
